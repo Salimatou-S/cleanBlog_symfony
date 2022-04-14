169a911e5bd4dfb0f9ff918b9302463c2dd22a36
Salimatou-S <mascindye2016@gmail.com>
2022-04-14 16:58:40 +0200

    reset-password, gestion des valeurs initiales des entités

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,6 +6,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
@@ -17,7 +18,10 @@ class Category
 
     #[ORM\Column(type: 'string', length: 100)]
     private $name;
-
+    
+    /**
+     * @gedmo\Slug(fields={"name"})
+     */
     #[ORM\Column(type: 'string', length: 120)]
     private $slug;
 
@@ -58,12 +62,12 @@ class Category
         return $this->slug;
     }
 
-    public function setSlug(string $slug): self
+   /*  public function setSlug(string $slug): self
     {
         $this->slug = $slug;
 
         return $this;
-    }
+    } */
 
     public function getParent(): ?self
     {
